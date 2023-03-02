b9d6028adc4457d91fa57ea0f9624b282e9e8d4a
achi mamukashvili <mk@gmail.com>
2023-03-02 21:07:41 +0400

    update

---
+++ index.js
@@ -0,0 +1,25 @@
+const ask = document.getElementsByClassName("asked");
+const arrow = document.getElementsByClassName("arrow-bottom");
+
+
+for(let i = 0; i < ask.length; i++) {
+    ask[i].addEventListener('click', (event)=> { 
+        let searchActive = document.querySelector('.active');
+        let h4;
+        if(searchActive) {
+            h4 = searchActive.firstElementChild.firstElementChild;
+        }
+        if(h4 == ask[i]) {
+           searchActive.classList.toggle('active');
+        } else {
+            searchActive?.classList.remove('active');
+            ask[i].parentElement.parentElement.classList.toggle('active');
+            console.log('elsshi shevida');
+        }
+
+        
+    
+    });
+    
+    
+}
